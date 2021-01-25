69cbf7e600fe8086ee1b34cb28458bcb8d1c1855
Sev <michal.zalewski1995@gmail.com>
2021-01-25 19:43:53 +0100

    Wyswietlenie okna ResourceSample

--- App.xaml.cs
+++ App.xaml.cs
@@ -16,9 +16,10 @@ namespace SpeechRecognition
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             MainWindow wnd = new MainWindow();
+            ResourceSample rsw = new ResourceSample();
             if (e.Args.Length == 1)
                 MessageBox.Show("Now opening file: \n\n" + e.Args[0]);
-            wnd.Show();
+            rsw.Show();
         }
     }
 }
