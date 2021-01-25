bb51b83cb7e6e547d75472ad4752a5e798b34147
Sev <michal.zalewski1995@gmail.com>
2021-01-25 19:28:07 +0100

    Wyswietlenie okna z parametrem wiersza polecen

--- App.xaml.cs
+++ App.xaml.cs
@@ -16,7 +16,8 @@ namespace SpeechRecognition
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             MainWindow wnd = new MainWindow();
-            wnd.Title = "Something else";
+            if (e.Args.Length == 1)
+                MessageBox.Show("Now opening file: \n\n" + e.Args[0]);
             wnd.Show();
         }
     }
