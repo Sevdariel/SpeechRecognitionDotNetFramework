0e002390cf73f7c84b02f12aa3701c5f9b3b57c9
Sev <michal.zalewski1995@gmail.com>
2021-01-27 21:57:52 +0100

    Formatowanie tekstu w TextBlock

--- Kontrolki/TextBlockInlineSample.xaml.cs
+++ Kontrolki/TextBlockInlineSample.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Navigation;
 
 namespace SpeechRecognition.Kontrolki
 {
@@ -10,6 +12,11 @@ namespace SpeechRecognition.Kontrolki
         public TextBlockInlineSample()
         {
             InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(e.Uri.AbsoluteUri);
         }
     }
 }
