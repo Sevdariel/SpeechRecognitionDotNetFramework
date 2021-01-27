b49243742143c3dba0bc7bc0e5c0bbbdc5dfcd03
Sev <michal.zalewski1995@gmail.com>
2021-01-27 23:05:27 +0100

    TextBox

---
+++ Kontrolki/TextBoxSample.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SpeechRecognition.Kontrolki
+{
+    /// <summary>
+    /// Interaction logic for TextBox.xaml
+    /// </summary>
+    public partial class TextBoxSample : Window
+    {
+        public TextBoxSample()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            txtStatus.Text = "Selection starts at character #" + textBox.SelectionStart + Environment.NewLine;
+            txtStatus.Text += "Selection is " + textBox.SelectionLength + " character(s) long" + Environment.NewLine;
+            txtStatus.Text += "Selected text: '" + textBox.SelectedText + "'";
+        }
+    }
+}
